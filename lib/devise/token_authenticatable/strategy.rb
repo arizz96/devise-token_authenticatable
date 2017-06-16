a95ca13432ce85ebfecb76b1b264f3f0dcaa01f4
arizz96 <arizz96@gmail.com>
2017-06-16 22:57:22 +0200

    Added tracking skip

--- lib/devise/token_authenticatable/strategy.rb
+++ lib/devise/token_authenticatable/strategy.rb
@@ -43,6 +43,7 @@ module Devise
         end
 
         if validate(resource)
+          env["devise.skip_trackable"] = true
           resource.after_token_authentication
           success!(resource)
         end
